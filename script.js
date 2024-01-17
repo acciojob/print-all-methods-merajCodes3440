5751fe1c3377e46fd323e3827d117f356904d62d
MerajCodes <merajsap184@gmail.com>
2024-01-17 12:56:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-//your JS code here. If required.
 function allMethods() {
-  //write your code here
-}
 
+  let methods  = "abs, acos, acosh, asin, asinh, atan, atan2, atanh, cbrt, ceil, clz32, cos, cosh, exp, expm1, floor, fround, hypot, imul, log, log10, log1p, log2, max, min, pow, random, round, sign, sin, sinh, sqrt, tan, tanh, trunc"
+  let methodArr  = methods.split(",")
+  return methodArr;
+  
+}
 alert(allMethods());
